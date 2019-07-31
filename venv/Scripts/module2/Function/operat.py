cc599263fe825091d153325cbe52f3eaf7673bac
Maxim Garmash <maxim.garmash@gmail.com>
2019-07-31 18:32:59 +0300

    Working with functions

---
+++ venv/Scripts/module2/Function/operat.py
@@ -0,0 +1,4 @@
+import operator as op
+# print(op.add(4, 5))
+# print(op.mul(4, 5))
+print(op.contains([1, 2, 3], 4))
--- venv/Scripts/module2/Function/sort.py
+++ venv/Scripts/module2/Function/sort.py
@@ -4,11 +4,16 @@ x = [
     ("John", "Backus")
 ]
 
-def length(name):
-    return len(" ".join(name))
+# def length(name):
+#     return len(" ".join(name))
+#
+# name_lengths = [length(name) for name in x]
+# print(name_lengths)
+#
+# x.sort(key=length)
+# print(x)
 
-name_lengths = [length(name) for name in x]
-print(name_lengths)
+# Эквивалентная запись
 
-x.sort(key=length)
+x.sort(key=lambda name: len(" ".join(name)))
 print(x)
