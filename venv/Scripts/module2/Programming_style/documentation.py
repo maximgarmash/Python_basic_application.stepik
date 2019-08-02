7a5b3243f6079dec118648f6322b387f1afb495c
Maxim Garmash <maxim.garmash@gmail.com>
2019-08-02 12:09:17 +0300

    Working with functions

---
+++ venv/Scripts/module2/Programming_style/documentation.py
@@ -0,0 +1,27 @@
+from random import random
+
+
+class RandomIterator:
+    """
+    RandomIterator(k) - new iterator for k random numbers in [0, 1)
+
+    Uses random.random
+    """
+    def __iter__(self):
+        return self
+
+    def __init__(self, k):
+        self.k = k
+        self.i = 0
+
+    def __next__(self):
+        if self.i < self.k:
+            self.i += 1
+            return random()
+        else:
+            raise StopIteration
+
+
+print(RandomIterator.__doc__)
+for x in RandomIterator(10):
+    print(x)
