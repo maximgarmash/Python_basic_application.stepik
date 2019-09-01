1dfee992f0c616f6814bd35dd6f5c8d14a66f78c
Maxim Garmash <maxim.garmash@gmail.com>
2019-09-01 16:35:07 +0300

    Add task1 and task2 in the module3-Internet

--- venv/Scripts/module3/Internet/Tasks/task1.py
+++ venv/Scripts/module3/Internet/Tasks/task1.py
@@ -1,11 +1,27 @@
 import requests
 import re
 
-urlA = input()
-# urlB = str(input())
+# urlA = "https://stepic.org/media/attachments/lesson/24472/sample1.html"
+# urlB = "https://stepic.org/media/attachments/lesson/24472/sample2.html"
+urlA = str(input())
+urlB = str(input())
+answer = "No"
+pattern1 = r'<a href="'+urlB+r'">.*\n*</a>'
+pattern2 = r'<a href="(.*)">.*\n*</a>'
 resA = requests.get(urlA)
-# resB = requests.get(urlB)
-print(resA.status_code)
+lst_url = re.findall(pattern2, resA.text)
+for url in lst_url:
+    resC = requests.get(url)
+    if re.search(pattern1, resC.text):
+        answer = "Yes"
+        break
+print(answer)
 
+
+
+
+# print(resA.content)
+# print(type(resA.text))
+# print(type(resA.content))
 # pattern = r"\b<a href="+urlA
 # print(pattern)
