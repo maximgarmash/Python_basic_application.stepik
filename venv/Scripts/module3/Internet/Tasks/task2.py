da7c89c08d319b81bad0adbffc1988fa1f112917
Maxim Garmash <maxim.garmash@gmail.com>
2019-09-02 18:15:47 +0300

    Add task2 new solution

--- venv/Scripts/module3/Internet/Tasks/task2.py
+++ venv/Scripts/module3/Internet/Tasks/task2.py
@@ -1,7 +1,8 @@
 import requests
 import re
 link_pattern = re.compile(r'<a[^>]*?href="(.*?)"[^>]*?>')
-site_pattern = re.compile()
+site_pattern = re.compile(r'//(.*?)/')
+site_lst = set()
 
 file_url = "https://stepic.org/media/attachments/lesson/24472/sample0.html"
 file_text = requests.get(file_url).text
@@ -9,6 +10,8 @@ with open("file_site.txt", "w+") as f:
     f.write(file_text)
     f.seek(0)
     for url in link_pattern.findall(file_text):
-        print(url)
+        site = site_pattern.search(url)
+        site_lst.add(site.group(1))
+    print(site_lst)
 
 
