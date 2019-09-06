fe8de32cafeaafbc86ef643d91809fb0bfd32828
Maxim Garmash <maxim.garmash@gmail.com>
2019-09-06 18:45:28 +0300

    Add task2 solution

--- venv/Scripts/module3/Internet/Tasks/task2.py
+++ venv/Scripts/module3/Internet/Tasks/task2.py
@@ -1,15 +1,12 @@
 import requests
 import re
-link_pattern = re.compile(r'''<a[^>]*?href=["'](.*?)["'][^>]*?>''')
+# link_pattern = re.compile(r'''<a[^>]*?href=["'](.*?)["'][^>]*?>''')
+link_pattern = re.compile(r'''<a[^>]*?href=["'](.*?)[?"'][^>]*?>''')
 site_pattern = re.compile(r'(\S+://)?([^/:]+)[/:]?')
 site_set = set()
 
 file_url = input()
 file_text = requests.get(file_url).text
-
-# with open("file_site.txt", "w+") as f:
-#     f.write(file_text)
-#     f.seek(0)
 
 for url in link_pattern.findall(file_text):
     site = site_pattern.search(url)
