5bd11628fc7323f359a03829d66fb59843933e0e
Maxim Garmash <maxim.garmash@gmail.com>
2019-09-06 18:05:07 +0300

    Add in task2 new pattern

--- venv/Scripts/module3/Internet/Tasks/task2.py
+++ venv/Scripts/module3/Internet/Tasks/task2.py
@@ -1,24 +1,22 @@
 import requests
 import re
-# link_pattern = re.compile(r'<a[^>]*?href="(.*?)"[^>]*?>')
 link_pattern = re.compile(r'''<a[^>]*?href=["'](.*?)["'][^>]*?>''')
-site_pattern = re.compile(r'(\S+://)?(\b^.[^/:]+)[/:]?')
-# site_pattern = re.compile(r'//(.*?)/')
-site_lst = set()
+site_pattern = re.compile(r'(\S+://)?([^/:]+)[/:]?')
+site_set = set()
 
-file_url = "https://stepic.org/media/attachments/lesson/24472/sample0.html"
+file_url = input()
 file_text = requests.get(file_url).text
-file_text = '<a href="http://neerc.ifmo.ru:1345">'
-# file_text = '<a href="ya.ru">'
-# file_text = '<a href="https://neerc.ifmo.ru">'
-# file_text = '<a href="../skip_relative_links">'
+
 # with open("file_site.txt", "w+") as f:
 #     f.write(file_text)
 #     f.seek(0)
+
 for url in link_pattern.findall(file_text):
     site = site_pattern.search(url)
-    print(site.groups())
-    site_lst.add(site.group(2))
-    print(site_lst)
+    if site.group(2) != '..':
+        site_set.add(site.group(2))
+
+for i in sorted(site_set):
+    print(i)
 
 
