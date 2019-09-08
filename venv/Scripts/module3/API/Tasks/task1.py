e82d48ea42bd01fae52a5f644442588cb88f8b1f
Maxim Garmash <maxim.garmash@gmail.com>
2019-09-08 14:52:07 +0300

    Add task1 solution in chapter 3.6

---
+++ venv/Scripts/module3/API/Tasks/task1.py
@@ -0,0 +1,13 @@
+import requests
+
+api_url = "http://numbersapi.com/"
+
+input_file = "dataset_24476_3.txt"
+with open(input_file) as in_f, open("output.txt", "w") as out_f:
+    for number in in_f.readlines():
+        res = requests.get(api_url+number.rstrip()+"/math", params="json")
+        data = res.json()
+        if data["found"]:
+            out_f.write("Interesting\n")
+        else:
+            out_f.write("Boring\n")
