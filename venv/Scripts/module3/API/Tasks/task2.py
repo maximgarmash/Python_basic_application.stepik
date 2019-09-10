c8c758100a52c4013d191c582aa2d9809b9ddb86
Maxim Garmash <maxim.garmash@gmail.com>
2019-09-10 18:22:22 +0300

    Add solution task2 in chapter 3.6

--- venv/Scripts/module3/API/Tasks/task2.py
+++ venv/Scripts/module3/API/Tasks/task2.py
@@ -1,18 +1,25 @@
 import requests
 import json
 
+input_file = "dataset_24476_4.txt"
+artists = dict()
 headers = {"X-Xapp-Token" : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsImV4cCI6MTU2ODYyNDQ3MywiaWF0IjoxNTY4MDE5NjczLCJhdWQiOiI1ZDc2MDZhODI4MTc2OTAwMTFhZDE2ZTAiLCJpc3MiOiJHcmF2aXR5IiwianRpIjoiNWQ3NjE0ZDk4MWFkZGEwMDExZWFlMGZiIn0.zpIEdsJoxmyfS0Zq2LlJcDKZ5s-RiOWYFrcPANtv2HI'}
 # инициируем запрос с заголовком
 
-input_file = "input2.txt"
-with open(input_file) as in_f, open("output2.txt", "w", encoding='UTF-8') as out_f:
+with open(input_file) as in_f:
     for artist_id in in_f.readlines():
         res = requests.get("https://api.artsy.net/api/artists/"+artist_id.rstrip(), headers=headers)
         data = json.loads(res.text)
-        out_f.write(data['sortable_name'] + ' ' + data['birthday'] + '\n')
+        birthday = data['birthday']
+        if artists.get(birthday):
+            artists[birthday].append(data['sortable_name'])
+        else:
+            artists[birthday] = [data['sortable_name']]
 
+for birth in sorted(artists.keys()):
+    for art in sorted(artists[birth]):
+        print(art)
 
-# разбираем ответ сервера
-# j = json.loads(r.text)
-# print(j['sortable_name'], j['birthday'])
+# for birthday in sorted(artists.values()):
+#     print(birthday)
 
