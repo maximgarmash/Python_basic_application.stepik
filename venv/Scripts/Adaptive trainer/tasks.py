2e01e18b9c64f62a2610d4e43ed8892418dc564e
Maxim Garmash <maxim.garmash@gmail.com>
2019-09-12 18:57:55 +0300

    Add task 4.3 - Caesar cipher

--- venv/Scripts/Adaptive trainer/tasks.py
+++ venv/Scripts/Adaptive trainer/tasks.py
@@ -5,9 +5,23 @@
 #     print(True)
 # else:
 #     print(False)
-
-x = int(input())
-print(x == -10 or -5 < x <=3 or 8 < x <12 or x >= 16)
+######################################################################
+# x = int(input())
+# print(x == -10 or -5 < x <=3 or 8 < x <12 or x >= 16)
 
 # 4.3 Caesar cipher
+
+alphabet = " abcdefghijklmnopqrstuvwxyz"
+shift = int(input())
+string = str(input())
+cipher_str = ''
+for character in string:
+    if alphabet.index(character)+shift > len(alphabet) - 1:
+        cipher_str += alphabet[abs(alphabet.index(character)+shift-(len(alphabet)-1))]
+    # elif alphabet.index(character)+shift < 0:
+    #     cipher_str += alphabet[alphabet.index(character)+shift-(len(alphabet)-1)]
+    else:
+        cipher_str += alphabet[alphabet.index(character)+shift]
+
+print("Result:", '"'+cipher_str+'"')
 
