a50e489d8b71c5d0bff3a2f2134e7605187efe7a
Maxim Garmash <maxim.garmash@gmail.com>
2019-09-17 09:13:57 +0300

    Add solution task 4.3 in Adaptive trainer

--- venv/Scripts/Adaptive trainer/4_3 Caesar cipher.py
+++ venv/Scripts/Adaptive trainer/4_3 Caesar cipher.py
@@ -6,10 +6,10 @@ string = str(input().strip())
 cipher_str = ''
 for character in string:
     if alphabet.index(character)+shift > len(alphabet) - 1:
-        cipher_str += alphabet[alphabet.index(character)+shift-len(alphabet)]
-    # elif alphabet.index(character)+shift < 0:
-    #     cipher_str += alphabet[alphabet.index(character)+shift-(len(alphabet)-1)]
+        cipher_str += alphabet[alphabet.index(character)+shift-len(alphabet)*((alphabet.index(character)+shift)//27)]
+    elif shift < 0:
+        cipher_str += alphabet[alphabet.index(character)-abs(shift) % 27]
     else:
-        cipher_str += alphabet[alphabet.index(character)+shift]
+        cipher_str += alphabet[alphabet.index(character) + shift]
 
 print("Result:", '"'+cipher_str+'"')
--- venv/Scripts/Adaptive trainer/tasks.py
+++ venv/Scripts/Adaptive trainer/tasks.py
@@ -1,13 +1,3 @@
-# 5.18 Union of intervals
-
-# x = int(input())
-# if x == -10 or -5 < x <=3 or 8 < x <12 or x >= 16:
-#     print(True)
-# else:
-#     print(False)
-######################################################################
-# x = int(input())
-# print(x == -10 or -5 < x <=3 or 8 < x <12 or x >= 16)
 
 # 4.3 Caesar cipher
 
@@ -17,11 +7,10 @@ string = str(input().strip())
 cipher_str = ''
 for character in string:
     if alphabet.index(character)+shift > len(alphabet) - 1:
-        cipher_str += alphabet[alphabet.index(character)+shift-len(alphabet)]
-    # elif alphabet.index(character)+shift < 0:
-    #     cipher_str += alphabet[alphabet.index(character)+shift-(len(alphabet)-1)]
+        cipher_str += alphabet[alphabet.index(character)+shift-len(alphabet)*((alphabet.index(character)+shift)//27)]
+    elif shift < 0:
+        cipher_str += alphabet[alphabet.index(character)-abs(shift) % 27]
     else:
-        cipher_str += alphabet[alphabet.index(character)+shift]
+        cipher_str += alphabet[alphabet.index(character) + shift]
 
 print("Result:", '"'+cipher_str+'"')
-
