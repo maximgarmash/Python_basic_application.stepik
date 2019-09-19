179e88df677049e79701b43874a4e2a1685658ac
Maxim Garmash <maxim.garmash@gmail.com>
2019-09-19 19:00:36 +0300

    Add new solution for task 3.7 in Adaptive trainer

--- venv/Scripts/Adaptive trainer/3_7 Multiple list index search.py
+++ venv/Scripts/Adaptive trainer/3_7 Multiple list index search.py
@@ -1,9 +1,21 @@
+# lst = [int(i) for i in input().split()]
+# n = int(input())
+# if n not in lst:
+#     print("None")
+# else:
+#     for dig in lst:
+#         if dig == n:
+#             print(lst.index(dig), end=" ")
+#             lst[lst.index(dig)] = dig + 1
+
 lst = [int(i) for i in input().split()]
 n = int(input())
-if n not in lst:
-    print("None")
-else:
+try:
+    x = lst.index(n)
     for dig in lst:
         if dig == n:
             print(lst.index(dig), end=" ")
             lst[lst.index(dig)] = dig + 1
+except ValueError:
+    print("None")
+
--- venv/Scripts/Adaptive trainer/test.py
+++ venv/Scripts/Adaptive trainer/test.py
@@ -1,5 +1,2 @@
-s = '1F600'
-print(chr(int(s, base=16)))
-alphabet = ''.join([chr(i) for i in range(int(0x1f600), int(0x1f64f)+1)])
-print(alphabet)
-print(len(alphabet))
+lst = [1, 2, 3]
+print(lst.index(4))
