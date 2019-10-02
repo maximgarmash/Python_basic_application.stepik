ee090faf46ada1d9aff7827c00a97e8d752b735e
Maxim Garmash <maxim.garmash@gmail.com>
2019-10-02 23:32:31 +0300

    Add solution num 1 in tasks 3.8 in Adaptive trainer

--- venv/Scripts/Adaptive trainer/3_8 Spiral matrix.py
+++ venv/Scripts/Adaptive trainer/3_8 Spiral matrix.py
@@ -19,4 +19,32 @@ Sample Output:
 13 12 11 10 9
 
 """
-n = input()
+
+
+def spiral(n, matrix, a, x):
+    for i in range(a, n):
+        matrix[a][i] = str(x)
+        x += 1
+    for i in range(a + 1, n):
+        matrix[i][n - 1] = str(x)
+        x += 1
+    for i in range(n - 2, a - 1, -1):
+        matrix[n - 1][i] = str(x)
+        x += 1
+    for i in range(n - 2, a, -1):
+        matrix[i][a] = str(x)
+        x += 1
+    n -= 1
+    a += 1
+    if n > 1:
+        spiral(n, matrix, a, x)
+    return matrix
+
+
+n = int(input())
+matrix = [[0 for j in range(n)] for i in range(n)]
+x, a = 1, 0
+
+ans_matrix = spiral(n, matrix, a, x)
+for i in range(n):
+    print(' '.join(ans_matrix[i]))
