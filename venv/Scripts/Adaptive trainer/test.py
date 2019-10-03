d3686198e3ad37335dfe9d7918280a948abb874e
Maxim Garmash <maxim.garmash@gmail.com>
2019-10-03 07:16:41 +0300

    Add reference in test

--- venv/Scripts/Adaptive trainer/test.py
+++ venv/Scripts/Adaptive trainer/test.py
@@ -1,3 +1,6 @@
 lst = [1, 2, 3]
 print(lst.index(4))
 print()
+
+# https://flask-russian-docs.readthedocs.io/ru/master/quickstart.html
+# https://the-bosha.ru/2016/06/03/python-flask-freimvork-pravilnaia-struktura-prilozheniia/
