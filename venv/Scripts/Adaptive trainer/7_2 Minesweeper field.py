5f6d506374bca9972a22f3fb16e2ee0e56736058
Maxim Garmash <maxim.garmash@gmail.com>
2019-10-17 15:41:56 +0300

    Add solution task 7.2

--- venv/Scripts/Adaptive trainer/7_2 Minesweeper field.py
+++ venv/Scripts/Adaptive trainer/7_2 Minesweeper field.py
@@ -32,9 +32,103 @@ Sample Output:
 """
 
 n, m = map(int, input().split())
-# print(n + m)
-mines_field = [input().split() for i in range(n)]
-print(mines_field)
-
+mines_field = [[j for j in str(input())] for i in range(n)]
+x, y = 1, 1
+if n == 1:
+    n += 1
+    x = 2
+if m == 1:
+    m += 1
+    y = 2
+format_mines_field = [[0 for j in range(m)] for i in range(n)]
+i = 0
 for row in mines_field:
+    j = 0
     for cell in row:
+        if cell == '*':
+            format_mines_field[i][j] = 1
+        j += 1
+    i += 1
+
+# print(mines_field)
+# print(format_mines_field)
+cleared_field = [[0 for j in range(m)] for i in range(n)]
+
+i = 0
+for row in mines_field:
+    j = 0
+    if i == 0:
+        for cell in row:
+            if j == 0:
+                if cell == '*':
+                    cleared_field[i][j] = '*'
+                else:
+                    cleared_field[i][j] = format_mines_field[i+1][j] + format_mines_field[i+1][j+1] + format_mines_field[i][j+1]
+            elif j != m-1:
+                if cell == '*':
+                    cleared_field[i][j] = '*'
+                else:
+                    cleared_field[i][j] = format_mines_field[i][j-1] +format_mines_field[i+1][j-1] + format_mines_field[i+1][j] + format_mines_field[i+1][j+1] + format_mines_field[i][j+1]
+            else:
+                if cell == '*':
+                    cleared_field[i][j] = '*'
+                else:
+                    cleared_field[i][j] = format_mines_field[i][j-1] + format_mines_field[i+1][j-1] + format_mines_field[i+1][j]
+            j += 1
+    elif i != n-1:
+        for cell in row:
+            if j == 0:
+                if cell == '*':
+                    cleared_field[i][j] = '*'
+                else:
+                    cleared_field[i][j] = format_mines_field[i-1][j] + format_mines_field[i-1][j+1] + format_mines_field[i][j+1] + format_mines_field[i+1][j+1] + format_mines_field[i+1][j]
+            elif j != m-1:
+                if cell == '*':
+                    cleared_field[i][j] = '*'
+                else:
+                    cleared_field[i][j] = format_mines_field[i-1][j-1] + format_mines_field[i-1][j] + format_mines_field[i-1][j+1] + format_mines_field[i][j+1] + format_mines_field[i+1][j+1] + format_mines_field[i+1][j] + format_mines_field[i+1][j-1] + format_mines_field[i][j-1]
+            else:
+                if cell == '*':
+                    cleared_field[i][j] = '*'
+                else:
+                    cleared_field[i][j] = format_mines_field[i-1][j] + format_mines_field[i-1][j-1] + format_mines_field[i][j-1] + format_mines_field[i+1][j-1] + format_mines_field[i+1][j]
+            j += 1
+    else:
+        for cell in row:
+            if j == 0:
+                if cell == '*':
+                    cleared_field[i][j] = '*'
+                else:
+                    cleared_field[i][j] = format_mines_field[i-1][j] + format_mines_field[i-1][j+1] + format_mines_field[i][j+1]
+            elif j != m - 1:
+                if cell == '*':
+                    cleared_field[i][j] = '*'
+                else:
+                    cleared_field[i][j] = format_mines_field[i][j-1] + format_mines_field[i-1][j-1] + format_mines_field[i-1][j] + format_mines_field[i-1][j+1] + format_mines_field[i][j+1]
+            else:
+                if cell == '*':
+                    cleared_field[i][j] = '*'
+                else:
+                    cleared_field[i][j] = format_mines_field[i][j-1] + format_mines_field[i-1][j-1] + format_mines_field[i-1][j]
+            j += 1
+    i += 1
+
+if x == 2:
+    del cleared_field[1]
+if y == 2:
+    i = 0
+    for row in cleared_field:
+        del cleared_field[i][1]
+        i += 1
+
+print('\n'.join([''.join([str(cell) for cell in row]) for row in cleared_field]))
+
+
+
+
+
+
+
+#
+#
+# print(cleared_field)
